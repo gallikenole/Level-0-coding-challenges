62dbc45da89566e0b3daf5e043f56873ec59d1b3
gallikenole <91693013+gallikenole@users.noreply.github.com>
2022-01-14 22:03:20 +0200

    Create task0.10.js

---
+++ task0.10.js
@@ -0,0 +1,15 @@
+function sameLettersCheck(string1,string2)
+{
+    for( var i = 0;i < string1.charAt;i++)
+    {
+        for( var j =0;j < string2.length();j++)
+        {
+            if(string1.charAt(i) == string2.charAt(j))
+            {
+                print(charAt);
+            }
+        }
+    }
+
+}
+console.log(sameLettersCheck("house","computers"))
