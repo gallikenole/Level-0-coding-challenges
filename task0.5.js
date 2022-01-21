ad602f4c13f86090ebde1a3cf9bad73d0a572e02
gallikenole <91693013+gallikenole@users.noreply.github.com>
2022-01-21 22:07:29 +0200

    Update task0.5.js

--- task0.5.js
+++ task0.5.js
@@ -1,9 +1,11 @@
 function AreaTriangle(side1,side2,side3)
 {
     
-  return s = (side1 + side2 + side3) / 2;
+   const s = (side1 + side2 + side3) / 2;
 
-  var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+   const area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+
+   return area
     
 }
     console.log(AreaTriangle(3,4,5));
