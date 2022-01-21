ea6b29b79a94ba1b0c6e94e02fe731be881ecab0
gallikenole <91693013+gallikenole@users.noreply.github.com>
2022-01-21 21:30:51 +0200

    Update task0.10.js

--- task0.10.js
+++ task0.10.js
@@ -2,7 +2,7 @@ function sameLettersCheck(string1,string2)
 {
     for( var i = 0;i < string1.charAt;i++)
     {
-        for( var j =0;j < string2.length();j++)
+        for( let j =0;j < string2.length();j++)
         {
             if(string1.charAt(i) == string2.charAt(j))
             {
