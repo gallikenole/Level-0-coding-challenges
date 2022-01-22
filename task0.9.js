d3d2523c2e1d6cb55f6b22fb44a6075c9026692c
gallikenole <91693013+gallikenole@users.noreply.github.com>
2022-01-22 22:22:31 +0200

    Update task0.9.js

--- task0.9.js
+++ task0.9.js
@@ -1,15 +1,20 @@
 function vowels(String)
-{
-    const vowellist= ["a","A","e","E","i","I","o","O","u","U"]
-    var count=0
-    for(let char of String)
-    {
-      if(vowellist.includes(char))
-      { 
-          return char;
-       
-      }   
+{   
+   let changeCase=String.toLowerCase()
+   let vowellist= []
     
+    for(i=0;i <changeCase.length;i++)
+    {
+      if (changeCase[i]=="a"||changeCase[i]=="e"||changeCase[i]=="i"||changeCase[i]=="o"||changeCase[i]=="u")
+      { 
+             
+       if(vowellist.indexOf(changeCase[i])==-1)  
+       {
+         vowellist.push(changeCase[i])
+       }    
     }
+   }
+
+    console.log(vowellist)
 }
-console.log(vowels("Umuzi"));
+      (vowels("Umuzi"))
